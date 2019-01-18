10d0d78edc1ac3ce3210d3da02d1cf7160a3c346
dev-pasa <dev5@uw.edu>
2019-01-17 23:47:08 -0800

    data

---
+++ Lab08-Manhattan/Classes/Features.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Lab08_Manhattan.Classes
+{
+    public class Features
+    {
+        [JsonProperty("properties")]
+        public Properties Properties { get; set; }
+
+        [JsonProperty("geometry")]
+        public Properties Geometry { get; set; }
+    }
+}
